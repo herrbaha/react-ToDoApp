4850b1e7fe760b3d58afe3d9f8b367968182d235
bahadir <baha_i@hotmail.com>
2021-09-14 21:01:58 +0200

    app js erstellt4

--- reacttodo/src/App.js
+++ reacttodo/src/App.js
@@ -13,10 +13,19 @@ tamamlandi:true}
 
 export default function App() {
   const [liste, setListe] = useState(INITIAL_STATE);
-  
+
   return (
     <div className="App">
-      hello world
+      <h1>Yapilacaklar Listesi</h1>
+      <div className="ekleme_formu">
+        <input placeholder="listeye ekle" />
+        <button>Ekle</button>
+      </div>
+      <div className="liste">
+        {liste.map(item => (
+          <div className="{item.tamamlandi}"></div>
+        ))}
+      </div>
     </div>
   );
 }
