fb294a2303e75335ff55d70a48f64e620f8484b6
bahadir <baha_i@hotmail.com>
2021-09-14 20:57:01 +0200

    app js erstellt

--- reacttodo/src/App.js
+++ reacttodo/src/App.js
@@ -1,7 +1,17 @@
 import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const INITIAL_STATE = [
+  { id: 1,
+  baslik:"alisveris yap",
+tamamlandi: false},
+{ id: 2,
+baslik:"fatura öde",
+tamamlandi:true}
+];
+
+
+export default function App() {
   return (
     <div className="App">
       hello world
