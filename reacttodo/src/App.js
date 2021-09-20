346bee96c2dee7dae496b3b71d046a061dd3b818
bahadir <baha_i@hotmail.com>
2021-09-20 10:09:28 +0200

    für setbaslik() eine erklärunn gerstellt

--- reacttodo/src/App.js
+++ reacttodo/src/App.js
@@ -12,7 +12,7 @@ export default function App() {
 
   const addNew = (title) => {
     setListe([...liste, { id: Date.now(), baslik: title, tamamlandi: false }]);//id.date.now() niye??  her yeni eklenen element icin ayri bir seye ihtiyacimiz var, bu hepsi icin farkli olmali, o yüzden burada saat kullanilmis bu farkli birsey de olabilirdi.
-    setYeniBaslik(""); 
+    setYeniBaslik(""); // eger bunu koymazsak yazdigimiz seyi kaydet dedigimizde input bosalmaz, inputun her seferinnde temizlenmesi icin bunu yapiyoruz
   };
 
   return (
