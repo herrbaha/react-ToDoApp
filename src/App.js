d28f9bae9a588d404c8f0af8002d2e55c423fe70
herrbaha <baha_i@hotmail.com>
2022-05-31 22:46:02 +0200

    formatting

--- src/App.js
+++ src/App.js
@@ -11,7 +11,10 @@ export default function App() {
   const [yeniBaslik, setYeniBaslik] = useState("");
 
   const addNew = (title) => {
-    setListe([...liste, { id: liste.length+1, baslik: title, tamamlandi: false }]);//id.date.now() veya liste.length+1 niye??  her yeni eklenen element icin ayri bir seye ihtiyacimiz var, bu hepsi icin farkli olmali, o yüzden burada saat kullanilmis bu farkli birsey de olabilirdi.
+    setListe([
+      ...liste,
+      { id: liste.length + 1, baslik: title, tamamlandi: false },
+    ]); //id.date.now() veya liste.length+1 niye??  her yeni eklenen element icin ayri bir seye ihtiyacimiz var, bu hepsi icin farkli olmali, o yüzden burada saat kullanilmis bu farkli birsey de olabilirdi.
     setYeniBaslik(""); // eger bunu koymazsak yazdigimiz seyi kaydet dedigimizde input bosalmaz, inputun her seferinnde temizlenmesi icin bunu yapiyoruz
   };
 
@@ -20,28 +23,36 @@ export default function App() {
       <h1>Yapilacaklar Listesi</h1>
       <div className="ekleme_formu">
         <input
-          value = {yeniBaslik}
-          onChange = {(e) => setYeniBaslik(e.target.value)}//e.target.value tam olarak nedir? inputun valuesuna ersimek icin herhalde ama niye böyle kullaniyliyor? inputun valuesuna ulasmak icin böyle kzllaniliyor istersek bu valuyu daha önce bir variabel olarak tanimlayip onu da kullanabiliriz
-          placeholder = "listeye ekle"
+          value={yeniBaslik}
+          onChange={(e) => setYeniBaslik(e.target.value)} //e.target.value tam olarak nedir? inputun valuesuna ersimek icin herhalde ama niye böyle kullaniyliyor? inputun valuesuna ulasmak icin böyle kzllaniliyor istersek bu valuyu daha önce bir variabel olarak tanimlayip onu da kullanabiliriz
+          placeholder="listeye ekle"
         />
-        <button onClick = {() => addNew(yeniBaslik)} > Ekle </button>
+        <button onClick={() => addNew(yeniBaslik)}> Ekle </button>
       </div>
       <div className="liste">
-        {liste.map((item, index) => (  //item ve element ayni listenin ayni elemanlari degil mi??
-          <div
-            key={index} //burada item.id de olabilirdi
-            onClick = {() => {
-              setListe(
-                liste.map((el) =>
-                  el.id === item.id ? { ...el, tamamlandi: !el.tamamlandi } : el  //!i burasi tam olarak nasil okunmali ??
-                )
-              );
-            }}
-            className={item.tamamlandi ? "yapildi" : ""}
-          >
-            {item.baslik}
-          </div>
-        ))}
+        {liste.map(
+          (
+            item,
+            index //item ve element ayni listenin ayni elemanlari degil mi??
+          ) => (
+            <div
+              key={index} //burada item.id de olabilirdi
+              onClick={() => {
+                setListe(
+                  liste.map(
+                    (el) =>
+                      el.id === item.id
+                        ? { ...el, tamamlandi: !el.tamamlandi }
+                        : el //!i burasi tam olarak nasil okunmali ??
+                  )
+                );
+              }}
+              className={item.tamamlandi ? "yapildi" : ""}
+            >
+              {item.baslik}
+            </div>
+          )
+        )}
         ;
       </div>
       <button
